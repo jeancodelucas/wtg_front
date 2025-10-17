0675521e7047f0b6bd35f88aa6d4bdb621ea9536
Jean Nascimento <seu.email@example.com>
2025-10-16 22:12:59 -0300

    Adição de mapa para capturar geolocalização do usuário

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,12 +1,17 @@
-import Flutter
 import UIKit
+import Flutter
+import GoogleMaps // 1. Importe a biblioteca do Google Maps
 
-@main
+@UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+
+    // 2. Adicione esta linha com a sua chave de API do iOS
+    GMSServices.provideAPIKey("AIzaSyDNE-vOY-834Xe9iN1DDTRYSDiAKK6QsLw")
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
